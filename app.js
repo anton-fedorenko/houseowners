d0ee5ccb0e4f23aa5d06f0222073fb01950ac5a0
Anton Fedorenko <Anton_Fedorenko@epam.com>
2021-09-01 19:16:21 +0300

    #1 Custom colors

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 module.exports = async function (fastify, opts) {
+    const path = require('path')
+    fastify.register(require('fastify-static'), {
+        root: path.join(__dirname, 'static'),
+        prefix: '/static/'
+    })
     fastify.register(require('point-of-view'), {
         engine: {
             nunjucks: require('nunjucks')
